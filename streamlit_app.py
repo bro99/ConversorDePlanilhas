78ad1940521090aecc45032d0e43b15917bc6ac8
bro99 <51249721+bro99@users.noreply.github.com>
2022-11-27 00:56:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import base64
+from io import BytesIO
 import streamlit as st
 import pandas as pd
 from PIL import Image
 image = Image.open('avatar.jfif')
+import xlsxwriter
 
 
 hide_streamlit_style = """
@@ -14,14 +16,24 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
 def get_table_download_link(df):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
     in:  dataframe
     out: href string
     """
-    val = df.to_excel(df)
+    val = to_excel(df)
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="MeusDados.xlsx">Clique em mim para baixar</a>' # decode b'abc' => abc
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+
+
 
 
 with st.sidebar:
